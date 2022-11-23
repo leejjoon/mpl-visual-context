4f50a885e771084c952117690854ae49d22f7b56
Jae-Joon Lee <lee.j.joon@gmail.com>
2022-11-23 16:36:56 +0900

    add initial version of axes_helper and spreader

---
+++ mpl_visual_context/axes_helper.py
@@ -0,0 +1,41 @@
+"""
+Helper class to tweak the visibility of various axes components.
+
+"""
+#Let's have a anoatomy of matplotlib guide this class.
+
+"""
+Axes
+    axis: left, right, ..
+      - ticks
+         - major
+         - minor
+      - ticklabels
+      - grid
+      - spine
+      - label
+    title
+    legend
+title
+legend
+"""
+
+from mpl_toolkits.axes_grid1.mpl_axes import (
+    Axes as _Axes,
+    SimpleAxisArtist as _SimpleAxisArtist
+)
+
+class SimpleAxisArtist(_SimpleAxisArtist):
+    def __init__(self, axis, axisnum, spine):
+        super().__init__(axis, axisnum, spine)
+        self.spine = self.line
+
+def get_axislines(ax):
+    axislines = _Axes.AxisDict(ax)
+    axislines.update(
+        bottom=SimpleAxisArtist(ax.xaxis, 1, ax.spines["bottom"]),
+        top=SimpleAxisArtist(ax.xaxis, 2, ax.spines["top"]),
+        left=SimpleAxisArtist(ax.yaxis, 1, ax.spines["left"]),
+        right=SimpleAxisArtist(ax.yaxis, 2, ax.spines["right"]))
+
+    return axislines
---
+++ mpl_visual_context/spreader.py
@@ -0,0 +1,26 @@
+import matplotlib.transforms as mtransforms
+
+def spready(artists, yindices, dy=None):
+    n = len(artists)
+    if dy is None:
+        dy = 1.
+    yoffsets = yindices * dy
+    for y, a in zip(yoffsets, artists):
+        tr = mtransforms.Affine2D().translate(0, y) + a.axes.transData
+        a.set_transform(tr)
+        a.set_zorder(a.get_zorder() - 0.001 * y)
+    return yoffsets
+
+    # gs.categories, gs.select("Poly")):
+
+
+# Use get_datalim instead
+def get_heights(artists):
+    yy = []
+    for a in artists:
+        pp = a.get_paths()
+        maxy = max([max(p.vertices[:, 1]) for p in pp])
+        yy.append(maxy)
+
+    return max(yy)
+
