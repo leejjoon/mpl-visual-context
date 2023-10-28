c28bfb74553c287172f2cf49a38351b6a4c76e9c
Jae-Joon Lee <lee.j.joon@gmail.com>
2023-10-28 17:36:03 +0900

    ArtistListWithPE respects zorder of artists by default

--- mpl_visual_context/artist_helper.py
+++ mpl_visual_context/artist_helper.py
@@ -30,13 +30,21 @@ def get_datalim(a):
 class ArtistListWithPE(Artist):
     """A simple container to filter multiple artists at once."""
 
-    def __init__(self, artist_list, pe):
+    def __init__(self, artist_list, pe, ignore_zorder=False):
         super().__init__()
         self._artist_list = artist_list
         self._pe = pe
+        self._ignore_zoder = ignore_zorder
 
     def draw(self, renderer):
-        for a in self._artist_list:
+        if self._ignore_zoder:
+            alist = self._artist_list
+        else:
+            sorted_alist = sorted((a.get_zorder(), i, a )
+                                  for i, a in enumerate(self._artist_list))
+            alist = [a for _, _, a in sorted_alist]
+
+        for a in alist:
             pe0 = a.get_path_effects()
             a.set_path_effects(self._pe)
             a.draw(renderer)
